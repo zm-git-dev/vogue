78995bbfeafe3fd163bcc9039776d4c57487a27f
Maya <maya.brandi@scilifelab.se>
2018-10-25 15:15:18 +0200

    adding more functions to fetch from lims

--- vogue/commands/lims.py
+++ vogue/commands/lims.py
@@ -5,30 +5,46 @@ db = client.trending
 from vogue.load.lims import MongoSample
 from argparse import ArgumentParser
 
+from mongo_adapter import (MongoAdapter, get_client)
+
+from genologics.lims import Lims
+from genologics.entities import Sample
+from genologics.config import BASEURI,USERNAME,PASSWORD
+lims = Lims(BASEURI,USERNAME,PASSWORD)
+
 DESC = ''
 
+class VougeAdapter(MongoAdapter):
+    def __init__(self,lims_sample_id):
+        self.sample_collection = self.db.sample
+    
+    
+    def add_sample(mongo_sample):
+        """Adds a sample to the database
+        
+        Args:
+            sample(dict)
 
-def build_sample(sample_id):
-    MS = MongoSample(sample_id)
-    MS.get_sample_level_data()
-    MS.get_concantration_and_nr_defrosts()
-    MS.get_sequenced_date()
-    MS.get_received_date()
-    MS.get_prepared_date()
-    MS.get_delivery_date()
-    MS.get_times()
+        Returns:
+            inserted_id(str)
+        """
+        # Code to add a sample
+        new_id = self.sample_collection.insert_one(mongo_sample).inserted_id
+
+
+def build_sample(sample):
+    MS = MongoSample(sample)
+
     new_id = db.sample.insert_one(MS.mongo_sample).inserted_id
 
 def build_all():
-    from genologics.lims import Lims
-    from genologics.config import BASEURI,USERNAME,PASSWORD
-    lims = Lims(BASEURI,USERNAME,PASSWORD)
     for sample in lims.get_samples():
-        build_sample(sample.id)
+        build_sample(sample)
 
 def main(args):
     if args.sample_id:
-        build_sample(args.sample_id)
+        sample = Sample(lims, id = args.sample_id)
+        build_sample(sample)
     else:
         build_all()        
 
