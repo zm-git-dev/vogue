4bcda7e251a0314a75069b65655d9cb129b81aee
Hassan Foroughi <hassanfa@users.noreply.github.com>
2018-11-06 14:28:26 +0100

    sure!
    Co-Authored-By: mayabrandi <maya.brandi@scilifelab.se>

--- vogue/commands/load/base.py
+++ vogue/commands/load/base.py
@@ -15,7 +15,7 @@ from vogue.tools.cli_utils import add_doc as doc
 
 @click.group()
 @click.version_option(version=__version__)
-@click.option('-d', '--database-name', default = 'trending', show_default = True, 
+@click.option('-d', '--database-name', type=click.Choice(['trending', 'trending_stage']), default = 'trending', show_default = True, 
                 help = 'Database name')
 @click.option('-u', '--database-uri', default = "mongodb://localhost:27017", 
                 show_default = True, help = 'Database uri')
