8c04e979f748716c555e44251e6bde08f1e198d9
Maya <maya.brandi@scilifelab.se>
2018-11-06 14:22:15 +0100

    making it more flat and adding more dry run info to the user

--- vogue/load/sample.py
+++ vogue/load/sample.py
@@ -13,8 +13,13 @@ def load_lims_sample(adapter, lims_id):
 
 def dry_run(adapter, lims_id):
     lims_sample = Sample(lims, id = lims_id)
-    mongo_sample = build_sample(lims_sample)
-    print(mongo_sample)
+    new_sample = build_sample(lims_sample)
+    existing_sample = adapter.sample_collection.find_one({'_id':lims_id})
+    if existing_sample:
+        print(f"The sample exist in the database and will be updated.")
+    else:
+        print(f"The sample does not exist in the database.")
+    print(f"Sample informamtion from lims to add/update: \n{new_sample}")
     
 def load_all_samples(adapter):
     for sample in lims.get_samples():
