47ce9f2e387b2be72bb38e8f010666f781222583
hassanfa <hassan.foroughi@gmail.com>
2018-11-09 15:26:59 +0100

    adds genologics and lims to load lims

--- vogue/commands/load/lims.py
+++ vogue/commands/load/lims.py
@@ -2,18 +2,25 @@ import logging
 import click
 from vogue.load.sample import load_one_sample, load_all_samples
 
+from genologics.lims import Lims
+from genologics.entities import Sample
+from genologics.config import BASEURI,USERNAME,PASSWORD
+
+LOG = logging.getLogger(__name__)
 
 @click.command("lims", short_help = "load lims into db.")
 @click.option('-s', '--sample-lims-id', help = 'Input sample lims id')
 @click.option('--load-all', is_flag = True, help = 'Loads all lims sample ids')
 @click.option('--dry', is_flag = True, help = 'Load from sample or not. (dry-run)')
 @click.pass_context
-
-
 def lims(context, sample_lims_id, dry, load_all):
     """Read and load lims data for a given sample id"""
     adapter = context.obj['adapter']
-    lims = context.obj['lims']
+    try:
+        lims = Lims(BASEURI,USERNAME,PASSWORD)
+    except Exception:
+        LOG.warning("Lims connection failed.")
+        context.abort() 
 
     if load_all:
         load_all_samples(adapter, lims=lims, dry_run=dry)
