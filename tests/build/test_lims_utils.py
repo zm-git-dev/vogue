35d567dc1ed5b9e7adf12f795d642d1498a9f9c9
Maya <maya.brandi@scilifelab.se>
2018-11-20 14:22:07 +0100

    Two new tests for test_get_latest_output_artifact

--- tests/build/test_lims_utils.py
+++ tests/build/test_lims_utils.py
@@ -1,5 +1,5 @@
 from datetime import datetime as dt
-from vogue.build.lims_utils import get_sequenced_date, get_number_of_days
+from vogue.build.lims_utils import get_sequenced_date, get_number_of_days, _get_latest_output_artifact
 
 
 def test_get_sequenced_date_no_udfs(lims_sample, lims):
@@ -33,20 +33,21 @@ def test_get_sequenced_date_no_artifacts(lims_sample, lims):
 
     assert sequenced_date is None
 
-def test_get_sequenced_date_one_artifact(lims_sample, lims, simple_artifact):
+def test_get_sequenced_date_one_artifact(lims_sample, lims):
     ##GIVEN a sample with udf: 'Passed Sequencing QC' and a lims with an artifact
     udf = 'Passed Sequencing QC'
-    date = '18-12-31'
+    date = '2018-12-31'
 
     lims_sample.udf[udf] = date
     assert lims_sample.udf.get(udf) == date
 
-    lims._add_artifact(simple_artifact)
+    process = lims._add_process(date_str = date)
+    artifact = lims._add_artifact(process)
     ##WHEN getting the sequence date
 
     sequenced_date = get_sequenced_date(lims_sample, lims)
 
-    ##THEN assert sequenced_date is none
+    ##THEN assert sequenced_date is datetime
 
     assert isinstance(sequenced_date, dt)
 
@@ -64,7 +65,7 @@ def test_get_number_of_days_no_date():
 
 
 def test_get_number_of_days():
-    ##GIVEN a two date time dates differing by two days
+    ##GIVEN two date time dates differing by two days
     first_date = dt.strptime('2018-05-31', '%Y-%m-%d')
     second_date = dt.strptime('2018-06-02', '%Y-%m-%d')
 
@@ -75,5 +76,38 @@ def test_get_number_of_days():
     assert nr_days == 2
 
 
-    
+def test_get_latest_output_artifact_no_art(lims):
+    ##GIVEN a lims with no artifacts
+
+    lims_id = 'Dummy'
+    process_type = 'CG002 - Aggregate QC (Library Validation)'
+
+    ##WHEN running _get_latest_output_artifact
+    latest_output_artifact = _get_latest_output_artifact(process_type, lims_id, lims)
+
+    ##THEN assert latest_output_artifact is none
+    assert latest_output_artifact is None
+
+
+def test_get_latest_output_artifact(lims):
+    ##GIVEN a lims with three artifacts with diferent parent processes with 
+    # diferent date_run dates 2018-01-01, 2018-02-01, 2018-03-01
+    lims_id = 'Dummy'
+    process_type = 'CG002 - Aggregate QC (Library Validation)'
+    date1 = '2018-01-01'
+    date2 = '2018-02-01'
+    date3 = '2018-03-01'
+    process1 = lims._add_process(date1, process_type)
+    process2 = lims._add_process(date2, process_type)
+    process3 = lims._add_process(date3, process_type)
+    artifact1 = lims._add_artifact(process1)
+    artifact2 = lims._add_artifact(process2)
+    artifact3 = lims._add_artifact(process3)
+
+    ##WHEN running _get_latest_output_artifact
+    latest_output_artifact = _get_latest_output_artifact(process_type, lims_id, lims)
+
+    ##THEN latest_output_artifact should be run on 2018-03-01
+    assert latest_output_artifact.parent_process.date_run == date3
+
 
