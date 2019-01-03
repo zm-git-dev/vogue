7426ea1c0ad3173cfdc0ff1bb01108c0b00f0be2
hassanfa <hassan.foroughi@gmail.com>
2019-01-03 14:15:17 +0100

    adds a new option to limit data load from json, reads and adds input json and semi-prepares dict

--- vogue/commands/load/analysis.py
+++ vogue/commands/load/analysis.py
@@ -1,34 +1,35 @@
 import logging
-
 import click
-
 import yaml
 import json
-
 from vogue.tools.cli_utils import json_read
 from vogue.tools.cli_utils import yaml_read
 from vogue.tools.cli_utils import check_file
 from vogue.build.analysis import validate_conf
+from vogue.build.analysis import build_analysis
+import vogue.models.analysis as m
 
 LOG_LEVELS = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
 LOG = logging.getLogger(__name__)
 
-@click.command("analysis", short_help = "Read files from analysis workflows")
-@click.option(
-    '-s',
-    '--sample-id',
-    required=True,
-    help='Input sample id'
-)
+
+@click.command("analysis", short_help="Read files from analysis workflows")
+@click.option('-s', '--sample-id', required=True, help='Input sample id')
 @click.option(
     '-a',
     '--analysis-config',
     type=click.Path(),
     required=True,
     help='Input config file. Accepted format: JSON, YAML')
+@click.option(
+    '-t',
+    '--analysis-type',
+    type=click.Choice(list(m.ANALYSIS_DESC.keys()) + ['all']),
+    multiple=True,
+    default='all',
+    help='Type of analysis results to load.')
 @click.pass_context
-
-def analysis(context, sample_id, analysis_config):
+def analysis(context, sample_id, analysis_config, analysis_type):
     """
     Read and load analysis results. These are either QC or analysis output files.
     """
@@ -40,10 +41,10 @@ def analysis(context, sample_id, analysis_config):
 
     LOG.info("Trying JSON format")
     analysis_dict = json_read(analysis_config)
-    if not isinstance(analysis_dict,dict):
+    if not isinstance(analysis_dict, dict):
         LOG.info("Trying YAML format")
         analysis_dict = yaml_read(analysis_config)
-        if not isinstance(analysis_dict,dict):
+        if not isinstance(analysis_dict, dict):
             LOG.error("Cannot read input analysis config file. Type unknown.")
             context.abort()
 
@@ -51,3 +52,23 @@ def analysis(context, sample_id, analysis_config):
     if not validate_conf(analysis_dict):
         LOG.error("Input config file is not valid format")
         context.abort()
+
+    ready_analysis = dict()
+    if analysis_type == 'all':
+        for a in m.ANALYSIS_DESC.keys():
+            tmp_analysis = build_analysis(analysis_dict, a)
+            if tmp_analysis:
+                ready_analysis = {**ready_analysis, **tmp_analysis}
+    else:
+        for a in analysis_type:
+            tmp_analysis = build_analysis(analysis_dict, a)
+            if tmp_analysis:
+                ready_analysis = {**ready_analysis, **tmp_analysis}
+
+    if ready_analysis:
+        LOG.info(
+            f'The following keys were found {list(ready_analysis.keys())}')
+    else:
+        LOG.warning(
+            f'No enteries were found for the given analysis type: {analysis_type}'
+        )
