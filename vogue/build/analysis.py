fa3108272bb321945ec15215375c48756ff06729
hassanfa <hassan.foroughi@gmail.com>
2019-01-03 14:13:24 +0100

    moves validate conf and reads json for load

--- vogue/build/analysis.py
+++ vogue/build/analysis.py
@@ -1,32 +1,47 @@
 import logging
 LOG = logging.getLogger(__name__)
 
+import vogue.models.analysis as m
 
-def validate_conf(analysis_conf):
+def validate_conf(analysis_conf: dict):
     """
     Takes input analysis_conf dictionary and validates entries.
 
     Checks for there are at least two keys in analysis_conf dictionary. If there is less than two, or the key doesn't
     exist, disqualifies the file and returns False
     """
+
     try:
-        if len(analysis_conf['report_saved_raw_data'].keys()) > 1:
-            LOG.info('Input multiqc file is probably properly formatted.')
-            valid_dict = True
+        if 'report_saved_raw_data' in analysis_conf.keys():
+            LOG.info('Input seems to be a valid multiqc report')
+            return True
         else:
-            LOG.warning(
-                'Input multiqc file has too few keys in it OR it is truncated.'
-            )
-            valid_dict = False
+            LOG.warning('Input does not seem to be a multiqc report')
+            return False
+
     except (KeyError, AttributeError) as e:
         LOG.warning(
             'Input multiqc file is either not a multiqc report or it it is truncated.'
         )
-        valid_dict = False
-
-    return valid_dict
+        return False
 
 
-def build_analysis(analysis: dict, analysis_type: str):
+def build_analysis(multiqc_dict: dict, analysis_type: str):
     """build a analysis object"""
-    return analysis
+
+    if analysis_type in m.ANALYSIS_SETS.keys():
+
+        analysis = dict()
+
+        # Find common analysis between predefined set and config file loaded.
+        analysis_sets = set(m.ANALYSIS_SETS[analysis_type].keys()) & set(
+            multiqc_dict['report_saved_raw_data'].keys())
+
+        for a in analysis_sets:
+            analysis[a] = multiqc_dict['report_saved_raw_data'][a]
+
+        return analysis
+
+    else:
+        LOG.warning(f'Analysis did not match the analysis type: {analysis_type}')
+        return None
