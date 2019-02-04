eb0c6ab03567770821be78a355a44c186e0cc37f
Maya <maya.brandi@scilifelab.se>
2019-02-04 14:39:30 +0100

    correcting GIVEN

--- tests/build/test_application_tag.py
+++ tests/build/test_application_tag.py
@@ -5,7 +5,7 @@ from vogue.exceptions import MissingApplicationTag
 
 
 def test_build_application_tag():
-    ## GIVEN a app_tag with missing requiered keys 'tag' and 'category'
+    ## GIVEN a app_tag with requiered keys 'tag' and 'category'
     app_tag = {'tag':'MELPCFR030', 'category':'wgs', 'gunnar': 25}
 
     ## WHEN building a mongo application tag
