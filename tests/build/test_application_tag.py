3675cf69eef7ddff456401b53dba0d956cda9035
Maya <maya.brandi@scilifelab.se>
2019-02-05 13:27:33 +0100

    fixing indentation errors

--- tests/build/test_application_tag.py
+++ tests/build/test_application_tag.py
@@ -4,23 +4,23 @@ from vogue.exceptions import MissingApplicationTag
 
 
 
- def test_build_application_tag():	
-    ## GIVEN a app_tag with requiered keys 'tag' and 'category'	
-    app_tag = {'tag':'MELPCFR030', 'category':'wgs', 'gunnar': 25}	
+def test_build_application_tag():	
+   ## GIVEN a app_tag with requiered keys 'tag' and 'category'	
+   app_tag = {'tag':'MELPCFR030', 'category':'wgs', 'gunnar': 25}	
 
-     ## WHEN building a mongo application tag	
-    mongo_application_tag = build_application_tag(app_tag)	
+   ## WHEN building a mongo application tag	
+   mongo_application_tag = build_application_tag(app_tag)	
 
-     ## THEN assert mongo_application_tag is {'_id':'MELPCFR030', 'category':'wgs'}	
-    assert mongo_application_tag ==  {'_id':'MELPCFR030', 'category':'wgs'}	
+   ## THEN assert mongo_application_tag is {'_id':'MELPCFR030', 'category':'wgs'}	
+   assert mongo_application_tag ==  {'_id':'MELPCFR030', 'category':'wgs'}	
 
 
- def test_build_application_tag_wrong_input():	
-    ## GIVEN a app_tag with missing requiered keys 'tag' and 'category'	
-    app_tag = {'tage':'ABC', 'categoryy':'wgs', 'gunnar': 25}	
+def test_build_application_tag_wrong_input():	
+   ## GIVEN a app_tag with missing requiered keys 'tag' and 'category'	
+   app_tag = {'tage':'ABC', 'categoryy':'wgs', 'gunnar': 25}	
 
-    ## WHEN building a mongo application tag	
-    ## THEN assert mongo_application_tag is None	
-    with pytest.raises(MissingApplicationTag):	
-        build_application_tag(app_tag)
+   ## WHEN building a mongo application tag	
+   ## THEN assert mongo_application_tag is None	
+   with pytest.raises(MissingApplicationTag):	
+      build_application_tag(app_tag)
 
