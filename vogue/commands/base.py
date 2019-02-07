2d44a37b75568af015ba4f95d27b54b5dc81f5e8
Maya <maya.brandi@scilifelab.se>
2019-02-07 08:33:04 +0100

    delete trash

--- vogue/commands/base.py
+++ vogue/commands/base.py
@@ -34,21 +34,6 @@ def name():
     click.echo(current_app.name)
     return current_app.name
 
-"""
-@click.group()
-@click.version_option(version=__version__)
-@click.option('--loglevel',
-    default='INFO',
-    type=click.Choice(LOG_LEVELS),
-    help="Set the level of log output.",
-    show_default=True,
-)
-@click.pass_context
-@doc("Vogue {version}: A trending package".format(version=__version__))
-def cli(context, loglevel):
-    coloredlogs.install(log_level=loglevel)
-cli.add_command(load_command)
-"""
 cli.add_command(test)
 cli.add_command(load)
 test.add_command(name)
--- vogue/commands/load/lims.py
+++ vogue/commands/load/lims.py
@@ -1,6 +1,8 @@
 import logging
 import click
 from vogue.load.sample import load_one_sample, load_all_samples
+from flask.cli import with_appcontext, current_app
+
 
 from genologics.lims import Lims
 from genologics.entities import Sample
@@ -14,18 +16,17 @@ LOG = logging.getLogger(__name__)
 @click.option('--dry', is_flag = True, help = 'Load from sample or not. (dry-run)')
 @click.option('-f', '--load-from', 
                 help = 'load from this sample lims id. Use if load all broke. Start where it ended')
-@click.pass_context
-def lims(context, sample_lims_id, dry, load_all, load_from):
+@with_appcontext
+def lims(sample_lims_id, dry, load_all, load_from):
     """Read and load lims data for a given sample id"""
-    adapter = context.obj['adapter']
     try:
         lims = Lims(BASEURI,USERNAME,PASSWORD)
     except Exception:
         LOG.warning("Lims connection failed.")
-        context.abort() 
+        raise click.Abort()
 
     if load_all:
-        load_all_samples(adapter, lims=lims, dry_run=dry, start_sample=load_from)
+        load_all_samples(current_app.adapter, lims=lims, dry_run=dry, start_sample=load_from)
         return
 
-    load_one_sample(adapter, sample_lims_id, lims=lims, dry_run=dry)
+    load_one_sample(current_app.adapter, sample_lims_id, lims=lims, dry_run=dry)
