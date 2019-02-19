0f465719e25698df7a66843e81a2f91a209a5289
Maya <maya.brandi@scilifelab.se>
2019-02-19 09:58:27 +0100

    Config working! Assuming MONGO_URI and MONGO_DBNAME as os.environ variables!

--- instance/config.py
+++ instance/config.py
@@ -1,13 +1,9 @@
-"""DEBUG = True
-
-SECRET_KEY = 'MySuperSecretKey'
-
-DB_URI =  "mongodb://localhost:27017"
-DB_NAME = 'trending'"""
-
 # encoding: utf-8
 import os
 
 # mongo
 DB_URI = os.environ['MONGO_URI']
 DB_NAME = os.environ['MONGO_DBNAME']
+
+DEBUG = True
+SECRET_KEY = 'MySuperSecretKey'
