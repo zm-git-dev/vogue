e331418908435de87ad413bfe01ae80c86fb82c4
Maya <maya.brandi@scilifelab.se>
2019-02-22 11:03:52 +0100

    dev

--- tests/build/test_build_sample.py
+++ tests/build/test_build_sample.py
@@ -1,17 +1,17 @@
 from vogue.build.lims import build_sample
 
-def test_build_sample(lims_sample, lims):
+def test_build_sample(lims_sample, lims, adapter):
     ## GIVEN a lims sample
     ## WHEN building a mongo sample
     lims_sample.udf['Sequencing Analysis'] = 'WGSLIF'
-    mongo_sample = build_sample(lims_sample, lims)
+    mongo_sample = build_sample(lims_sample, lims, adapter)
     ## THEN the sample should have been parsed in the correct way
     assert mongo_sample['_id'] == lims_sample.id
 
-def test_build_sample_family(family_sample, lims):
+def test_build_sample_family(family_sample, lims, adapter):
     ## GIVEN a lims sample with a family
     ## WHEN building a mongo sample
-    mongo_sample = build_sample(family_sample, lims)
+    mongo_sample = build_sample(family_sample, lims, adapter)
     ## THEN the sample should have been parsed in the correct way
     assert mongo_sample['family'] == family_sample.udf.get('Family')
 
