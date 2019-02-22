320c134f2926e70935641d3c5cbb67e8bcd171bb
hassanfa <hassan.foroughi@gmail.com>
2019-02-22 10:58:28 +0100

    draft for constructing a adapter for loading

--- vogue/load/analysis.py
+++ vogue/load/analysis.py
@@ -1,16 +1,33 @@
-from vogue.adapter import VougeAdapter
+import logging
+
+from genologics.entities import Sample
+from genologics.lims import Lims
+
 from vogue.build.analysis import build_analysis
 
-def load_cancer_analysis(adapter: VougeAdapter, lims_id=None, dry_run=False, analysis : dict={}):
+LOG = logging.getLogger(__name__)
+
+def load_analysis(adapter: VougeAdapter, lims_id=None, dry_run=False, analysis : dict={}):
     """Load information from a cancer analysis"""
     
-    sample_obj = adapter.sample(lims_id)
-    if not sample_obj:
-        raise SyntaxError("Sample {} does not exist".format(lims_id))
-    
-    ## TODO build the analysis object
-    analysis_obj = build_analysis(analysis, 'QC' )
-    ## TODO load the object with adapter
-    adapter.load_analysis(analysis_obj)
-    
-    return analysis_obj
+#    sample_obj = adapter.sample(lims_id)
+#    if not sample_obj:
+#        raise SyntaxError("Sample {} does not exist".format(lims_id))
+#    
+#    ## TODO build the analysis object
+#    analysis_obj = build_analysis(analysis, 'QC' )
+#    ## TODO load the object with adapter
+#    adapter.load_analysis(analysis_obj)
+#    
+#    return analysis_obj
+
+    if dry_run:
+        existing_sample = adapter.sample(lims_sample.id)
+        if existing_sample:
+            LOG.info("The sample exists in the database")
+        else:
+            LOG.info("The sample does not exist in the database")
+
+        LOG.info("Sample information from analysis to add/update: \n %s", mongo_sample)
+        return
+
