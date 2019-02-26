5af1f58a0528427050a475862b1461088ddcda4c
hassanfa <hassan.foroughi@gmail.com>
2019-02-26 12:44:53 +0100

    refactor(analysis): unused input cleanup

--- vogue/load/analysis.py
+++ vogue/load/analysis.py
@@ -7,20 +7,9 @@ from vogue.build.analysis import build_analysis
 
 LOG = logging.getLogger(__name__)
 
-def load_analysis(adapter, lims_id=None, dry_run=False, analysis : dict={}):
+def load_analysis(adapter, dry_run=False, analysis : dict={}):
     """Load information from a cancer analysis"""
     
-#    sample_obj = adapter.sample(lims_id)
-#    if not sample_obj:
-#        raise SyntaxError("Sample {} does not exist".format(lims_id))
-#    
-#    ## TODO build the analysis object
-#    analysis_obj = build_analysis(analysis, 'QC' )
-#    ## TODO load the object with adapter
-#    adapter.load_analysis(analysis_obj)
-#    
-#    return analysis_obj
-
     if dry_run:
         existing_sample = adapter.sample(lims_sample.id)
         if existing_sample:
