68ba66d55abfb5c7c8963fee858505a4682f5a3b
hassanfa <hassan.foroughi@gmail.com>
2019-02-26 15:40:47 +0100

    refactor(analysis): _sample_ changed to _analysis_

--- vogue/load/analysis.py
+++ vogue/load/analysis.py
@@ -7,17 +7,17 @@ from vogue.build.analysis import build_analysis
 
 LOG = logging.getLogger(__name__)
 
-def load_analysis(adapter, dry_run=False, analysis : dict={}):
+def load_analysis(adapter, lims_id, dry_run=False, analysis : dict={}):
     """Load information from a cancer analysis"""
     
     if dry_run:
-        existing_sample = adapter.sample(lims_sample.id)
-        if existing_sample:
+        existing_analysis = adapter.sample(lims_id)
+        if existing_analysis:
             LOG.info("The sample exists in the database")
         else:
             LOG.info("The sample does not exist in the database")
 
-        LOG.info("Sample information from analysis to add/update: \n %s", mongo_sample)
+        LOG.info("Analysis information for sample %s to add/update: \n %s", lims_id, analysis)
         return
 
     adapter.add_or_update_analysis(analysis)
