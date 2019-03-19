3dcb43560814e0767fdc68df06ec9cd52b2a9c9f
Maya <maya.brandi@scilifelab.se>
2019-03-19 07:42:36 +0100

    adding some options to the load

--- vogue/commands/load/lims.py
+++ vogue/commands/load/lims.py
@@ -1,6 +1,6 @@
 import logging
 import click
-from vogue.load.sample import load_one_sample, load_all_samples
+from vogue.load.sample import load_one, load_all, load_one_dry, load_all_dry
 from flask.cli import with_appcontext, current_app
 
 
@@ -11,13 +11,20 @@ from genologics.config import BASEURI,USERNAME,PASSWORD
 LOG = logging.getLogger(__name__)
 
 @click.command("lims", short_help = "load lims into db.")
-@click.option('-s', '--sample-lims-id', help = 'Input sample lims id')
-@click.option('--load-all', is_flag = True, help = 'Loads all lims sample ids')
-@click.option('--dry', is_flag = True, help = 'Load from sample or not. (dry-run)')
+@click.option('-s', '--sample-lims-id', 
+                help = 'Input sample lims id')
+@click.option('-a', '--all-samples', is_flag = True, 
+                help = 'Loads all lims sample ids')
+@click.option('--dry', is_flag = True, 
+                help = 'Load from sample or not. (dry-run)')
 @click.option('-f', '--load-from', 
                 help = 'load from this sample lims id. Use if load all broke. Start where it ended')
+@click.option('-n', '--new_only', is_flag = True,
+                help = 'Use this flagg if you only want to load samples that dont exist in the database')
+@click.option('-d', '--date', 
+                help = 'Update only samples delivered after date')
 @with_appcontext
-def lims(sample_lims_id, dry, load_all, load_from):
+def lims(sample_lims_id, dry, all_samples, load_from, new_only, date):
     """Read and load lims data for a given sample id"""
     try:
         lims = Lims(BASEURI,USERNAME,PASSWORD)
@@ -25,8 +32,20 @@ def lims(sample_lims_id, dry, load_all, load_from):
         LOG.warning("Lims connection failed.")
         raise click.Abort()
 
-    if load_all:
-        load_all_samples(current_app.adapter, lims=lims, dry_run=dry, start_sample=load_from)
-        return
+    if all_samples:
+        if dry:
+            load_all_dry()
+        else:
+            load_all(current_app.adapter, lims=lims, start_sample=load_from, new_only=new_only, date=date)
+    else:
+        lims_sample = Sample(lims, id = sample_lims_id)
+        if not lims_sample:
+            LOG.critical("The sample does not exist in the database in the LIMS database.")
+            raise SyntaxError()
+        if dry:
+            load_one_dry(lims_sample)
+        else:
+            load_one(current_app.adapter, lims_sample , lims=lims, new_only=new_only, date = date)
 
-    load_one_sample(current_app.adapter, sample_lims_id, lims=lims, dry_run=dry)
+
+
