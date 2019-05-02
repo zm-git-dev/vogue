52ab560d580955e114bb80042a5038da2161b12a
hassanfa <hassan.foroughi@gmail.com>
2019-05-02 15:43:27 +0200

    chore: removed unnecessary import

--- vogue/load/analysis.py
+++ vogue/load/analysis.py
@@ -3,8 +3,6 @@ import logging
 from genologics.entities import Sample
 from genologics.lims import Lims
 
-from vogue.build.analysis import build_analysis
-
 LOG = logging.getLogger(__name__)
 
 def load_analysis(adapter, lims_id, dry_run=False, analysis : dict={}):
