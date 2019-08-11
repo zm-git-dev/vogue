fd8db6ba1c8edde52404f717e047fe337430c530
hassanfa <hassan.foroughi@gmail.com>
2019-08-11 20:10:16 +0200

    cleanups code and adds cleanup-processed option

--- vogue/commands/load/case_analysis.py
+++ vogue/commands/load/case_analysis.py
@@ -21,16 +21,12 @@ LOG = logging.getLogger(__name__)
 
 
 @click.command("analysis", short_help="Read files from analysis workflows")
-@click.option('-s',
-              '--sample-id',
-              required=True,
-              multiple=True,
-              help='Input sample id.')
+@click.option('--sample-list',
+              help='Input list of comma separated sample names.')
 @click.option('-a',
               '--analysis-config',
               type=click.Path(),
               multiple=True,
-              required=True,
               help='Input config file. Accepted format: JSON, YAML')
 @click.option(
     '-t',
@@ -51,9 +47,12 @@ LOG = logging.getLogger(__name__)
 @click.option('--workflow-version',
               required=True,
               help='Analysis workflow used.')
-@click.option('--is-case',
+@click.option('--processed/--not-processed',
               is_flag=True,
-              help='Specify this flag if input json is case level.')
+              help='Specify this flag if input json is processed data.')
+@click.option('--cleanup/--not-cleanup',
+              is_flag=True,
+              help='Specify this flag if input json should be cleanup based on analysis-type.')
 @click.option('--case-analysis-type',
               type=click.Choice(['multiqc', 'custom']),
               default='multiqc',
@@ -66,94 +65,111 @@ LOG = logging.getLogger(__name__)
     analysis model. Analysis types recognize the following keys in the input file: {" ".join(concat_dict_keys(analysis_model.ANALYSIS_SETS,key_name=""))}
         """)
 @with_appcontext
-def analysis(sample_id, dry, analysis_config, analysis_type, analysis_case,
-             analysis_workflow, workflow_version, is_case, case_analysis_type):
+def analysis(dry, analysis_config, analysis_type, analysis_case,
+             analysis_workflow, workflow_version, processed, case_analysis_type, sample_list, cleanup):
 
-    # is_case does not work with multiple input analysis configs
-    if is_case and len(analysis_config) > 1:
-        LOG.error("is_case flag cannot be used with multiple input files")
+    if not analysis_config and not processed:
+        LOG.error('Either --analysis-config or --processed should be provided') 
         raise click.Abort()
-
-    if is_case and not case_analysis_type:
-        LOG.error("is_case flag requires a case_analysis_type value")
-        raise click.Abort()
-
-    if not is_case and len(sample_id) > 1:
-        LOG.error("for standard input, only use single sample ids.")
-        raise click.Abort()
-
-    if not is_case:
-        sample_id = copy.deepcopy(sample_id[0])
 
     analysis_dict = dict()
+
+    if sample_list:
+        sample_id = sample_list.split(',')
+    else:
+        if 'sample' not in analysis_dict.keys():
+            LOG.error('sample key not found in input json. Use --sample-list instead')
+            raise click.Abort()
+        # store sample_id from dict to avoid losing it downstream cleanup
+        sample_id = analysis_dict['sample']
 
     #if is_case flag is enabled, build dictionary without merging.
     # Loop over list of input config files for single sample and merge them into
     # one single dictionary
-    for input_config in analysis_config:
 
-        LOG.info("Reading and validating config file: %s", input_config)
-        try:
-            check_file(input_config)
-        except FileNotFoundError:
-            raise click.Abort()
+    if not processed and analysis_config:
+        for input_config in analysis_config:
 
-        LOG.info("Trying JSON format")
-        tmp_analysis_dict = json_read(input_config)
-
-        if not isinstance(analysis_dict, dict):
-            LOG.info("Trying YAML format")
-            tmp_analysis_dict = yaml_read(input_config)
-            if not isinstance(tmp_analysis_dict, dict):
-                LOG.error(
-                    "Cannot read input analysis config file. Type unknown.")
+            LOG.info("Reading and validating config file: %s", input_config)
+            try:
+                check_file(input_config)
+            except FileNotFoundError:
                 raise click.Abort()
 
-        analysis_dict = {**analysis_dict, **tmp_analysis_dict}
+            LOG.info("Trying JSON format")
+            tmp_analysis_dict = json_read(input_config)
 
-    analysis_dict = dict_replace_dot(analysis_dict)
+            if not isinstance(analysis_dict, dict):
+                LOG.info("Trying YAML format")
+                tmp_analysis_dict = yaml_read(input_config)
+                if not isinstance(tmp_analysis_dict, dict):
+                    LOG.error(
+                        "Cannot read input analysis config file. Type unknown.")
+                    raise click.Abort()
 
-    if not is_case:
-        LOG.info("Validating parsed config file(s).")
-        valid_analysis = validate_conf(analysis_dict)
-        if valid_analysis is None:
-            LOG.error("Invalid or badly formatted file(s).")
-            raise click.Abort()
-    else:
+            analysis_dict = {**analysis_dict, **tmp_analysis_dict}
+
+        analysis_dict = dict_replace_dot(analysis_dict)
+
+        # Get current sample if any
         old_keys = list(analysis_dict.keys())
         analysis_dict[case_analysis_type] = copy.deepcopy(analysis_dict)
-        valid_analysis = dict()
         for key in old_keys:
             analysis_dict.pop(key)
 
+    valid_analysis = dict()
     analysis_dict['case'] = analysis_case
     analysis_dict['workflow'] = analysis_workflow
     analysis_dict['workflow_version'] = workflow_version
     analysis_dict['sample'] = sample_id
+    analysis_dict['case_analysis_type'] = case_analysis_type
 
-    # Get current sample if any
-    if not is_case:
-        current_analysis = current_app.adapter.sample_analysis(sample_id)
+    if processed:
+        current_analysis = current_app.adapter.bioinfo_raw(analysis_case) 
+
+        if current_analysis is None:
+            LOG.info("Raw import for this case does not exist. Load it without processed flag first")
+            raise click.Abort()
+        elif not case_analysis_type in current_analysis['case_analysis_types']:
+            LOG.info("%s doesn't exist for case %s", case_analysis_type, analysis_case) 
+            raise click.Abort()
+
+        analysis_dict[case_analysis_type] = current_analysis[analysis_workflow][case_analysis_type][-1]
+
+        if cleanup:
+            LOG.info("Validating parsed config file(s).")
+            valid_analysis = validate_conf(analysis_dict[case_analysis_type])
+            if valid_analysis is None:
+                LOG.error("Invalid or badly formatted file(s).")
+                raise click.Abort()
+
+        current_analysis = current_app.adapter.bioinfo_processed(analysis_case)
+
     else:
-        current_analysis = current_app.adapter.case_analysis(analysis_case)
 
+        # Don't process the case
+        current_analysis = current_app.adapter.bioinfo_raw(analysis_case)
+        processed=False
+        cleanup=False
+
+    # Don't process the case
     ready_analysis = build_analysis(analysis_dict=analysis_dict,
                                     analysis_type=analysis_type,
                                     valid_analysis=valid_analysis,
                                     current_analysis=current_analysis,
-                                    build_case=is_case)
+                                    process_case=processed,
+                                    cleanup=cleanup)
 
-    if ready_analysis and not is_case:
+    if ready_analysis:
         LOG.info('Values for %s  loaded for sample %s',
                  list(ready_analysis.keys()), sample_id)
-    elif not ready_analysis and not is_case:
+    else:
         LOG.warning('No enteries were found for the given analysis type: %s',
                     analysis_type)
-    else:
-        LOG.info('Case %s will be added/updated', analysis_case)
+    LOG.info('Case %s will be added/updated', analysis_case)
 
     load_analysis(adapter=current_app.adapter,
                   lims_id=sample_id,
-                  is_case=is_case,
+                  processed=processed,
                   dry_run=dry,
                   analysis=ready_analysis)
