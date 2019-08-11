62754d9f7e6372f1c95a40bfa7b50af3a0a5ff45
hassanfa <hassan.foroughi@gmail.com>
2019-08-11 20:10:47 +0200

    replaces is_case with processed

--- vogue/load/case_analysis.py
+++ vogue/load/case_analysis.py
@@ -3,7 +3,7 @@ import logging
 LOG = logging.getLogger(__name__)
 
 
-def load_analysis(adapter, lims_id, analysis, is_case, dry_run=False):
+def load_analysis(adapter, lims_id, analysis, processed=False, dry_run=False):
     """Load information from a cancer analysis"""
 
     if dry_run:
@@ -16,8 +16,8 @@ def load_analysis(adapter, lims_id, analysis, is_case, dry_run=False):
         LOG.info("Analysis information for sample %s to add/update: \n %s",
                  lims_id, analysis)
         return
-
-    if is_case:
-        adapter.add_or_update_analysis_case(analysis)
+    
+    if not processed:
+        adapter.add_or_update_analysis_bioinfo_raw(analysis)
     else:
-        adapter.add_or_update_analysis_sample(analysis)
+        adapter.add_or_update_analysis_bioinfo_processed(analysis)
