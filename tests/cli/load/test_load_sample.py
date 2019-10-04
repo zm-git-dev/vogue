76d290e8f216d1ba647d9b32042327eab69f946a
Maya <maya.brandi@scilifelab.se>
2019-10-04 04:39:58 +0200

    adding test

--- tests/cli/load/test_load_sample.py
+++ tests/cli/load/test_load_sample.py
@@ -39,6 +39,40 @@ def test_sample_days(database, lims):
     # THEN the sample was added
     assert app.adapter.sample_collection.count() == 1
 
+def test_sample_wrong_days2(database, lims):
+    # GIVEN a app context with a mock lims with a process 
+
+    # database with a apptag in the application tag collection:
+    database.application_tag.update_one({'_id' : 'WGSPCFC030'}, 
+                                        {'$set' : {"category" : "wgs"}},
+                                         upsert=True)
+
+    # a lims with a lims sample
+    lims_sample = lims._add_sample(sample_id='sample')                                
+    lims_sample.udf['Sequencing Analysis'] = 'WGSPCFC030'
+
+    # and a artifact related to the sample
+    artifact = lims._add_artifact(samples = [lims_sample])
+
+    # that was input to a process that was modifyed 3 days ago
+    process = lims._add_process()
+    process.inputs.append(artifact)
+    three_days_ago = date.today() - timedelta(days=3)
+    process.modified = three_days_ago.strftime("%Y-%m-%dT00:00:00Z")
+
+
+    adapter = VougeAdapter(database.client, db_name = database.name)
+    app.adapter = adapter
+    app.db = database
+    app.lims = lims
+
+    # WHEN trying to tun the cli with incorect argument format for -d 
+    runner = app.test_cli_runner()
+    result = runner.invoke(cli, ['load', 'sample', '-d', 'i'])
+
+    # THEN the program exits
+    assert result.exit_code == 2
+
 def test_sample_wrong_days():
     # GIVEN a app context
 
