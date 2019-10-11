60f67318fcda8a69d019a81b27623caa343375cd
mayabrandi <maya.brandi@scilifelab.se>
2019-10-11 15:42:18 +0200

    Update views.py

--- vogue/server/views.py
+++ vogue/server/views.py
@@ -7,7 +7,7 @@ from vogue.server.utils import *
 
 
 app = current_app
-blueprint = Blueprint('server', __name__)
+blueprint = Blueprint('server', static_folder='static',  __name__)
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
