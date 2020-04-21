19e8dfe6dbfa5e110ae034f623da212d026c1ed6
Maya <maya.brandi@scilifelab.se>
2020-04-21 14:37:18 +0200

    finally

--- vogue/server/__init__.py
+++ vogue/server/__init__.py
@@ -16,16 +16,18 @@ logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
 
-
-
 def create_app(test=False):
     app = Flask(__name__)
     app.test = test
+    if test:
+        return app
+
     try:
         app.config.from_envvar('VOGUE_CONFIG')
         configure_app(app)
     except:
         pass
+
     return app
 
 def configure_app(app, config=None):
@@ -36,7 +38,7 @@ def configure_app(app, config=None):
 
     if config:
         app.config = {**app.config, **yaml.safe_load(config)}
-        
+
     client = MongoClient(app.config['DB_URI'])
     db_name = app.config['DB_NAME']
     app.client = client
